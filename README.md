f9b123e83e0b6658f815f748c93364a29e6a160c
sabik <sabik@mandroslabs.com>
2014-07-31 23:12:37 -0500

    mongonizin

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
 Gmail to JSON
 =====
  
-Command line PHP tool for exporting all Inbox in Gmail to separate .JSON text files
+Command line PHP tool for exporting all Inbox in Gmail to JSON text files
 
 Requirements:
 
-- Command line access to PHP
+- Command line interface access to PHP cli
 - IMAP configured in PHP cli 
+- Access to ```system('stty echo')``` since it is used to hide login password
+- MongoDB driver for PHP cli (if exporting to mongo)
 
-Usage:
+Ubuntu / Debian setup instructions
+
+```
+sudo apt-get install php5
+sudo apt-get install php5-imap
+sudo gedit /etc/php5/cli/php.ini
+```
+
+On php.ini add extension ```imap.so```, if you are using MongoDB ```mongo.so``` should be somewhere in there also.
+
+Usage
 
 - ```php gmail-json.php```
 - Write login credentials
+- Follow the wizard
 - Wait for the fun
 
-Will output files to the directory ./json
+If you export to MongoDB you may like to perform indexing to do some querying or operations with the text
+
+```
+db.gmail.ensureIndex( { from: "text" } )
+db.gmail.ensureIndex( { subject: "text" } )
+db.gmail.ensureIndex( { message: "text" } )
+```
--- gmail-json.php
+++ gmail-json.php
@@ -13,96 +13,99 @@ system('stty echo');
 print PHP_EOL;
 
 try {
-    $inbox = imap_open($hostname, $user, $pass) or die('IMAP / GMail not working: ' . imap_last_error());
+    $inbox = imap_open($hostname, $user, $pass) or die('IMAP / Gmail not working: ' . imap_last_error());
 } catch (Exception $e) {
     print 'Perhaps IMAP for PHP missing?' . PHP_EOL . $e;
 }
 $emails = imap_search($inbox, 'ALL');
 
-
 # General wizard
-$outputdir = './json';
-$many_files = 'Y';
-$write_mongo = false;
-
-
-print 'Output directory (default is '. $outputdir .' ): ';
+$outputdir = './gmail-backup';
+$write_mongo = null;
+print 'Output directory (default is '. $outputdir . '): ';
 $response = trim(fgets(STDIN));
-if($response != '')
+if($response != '') {
     $outputdir = $response;
-
-print 'Output many files (default is Y): ';
-$response = trim(fgets(STDIN));
-if($response != '')
-    $many_files = $response;
-if($many_files == 'N' || $many_files == 'n')
-    $mails_array = array();
-else
-    $mails_array = false;
-
+}
 print 'Write to MongoDB (default is N): ';
 $response = trim(fgets(STDIN));
 if($response != '')
     $write_mongo = $response;
 
 #  MongoDB wizard
-if($write_mongo) {
-    print 'MongoDB connection string: ';
-    $user = trim(fgets(STDIN));
-    print 'MongoDB login: ';
-    $user = trim(fgets(STDIN));
-    print 'MongoDB database: ';
-    $user = trim(fgets(STDIN));
-    print 'MongoDB collection: ';
-    $user = trim(fgets(STDIN));
-    print 'Gmail user: ';
-    $user = trim(fgets(STDIN));
-}
+$mogo_url = 'localhost:27017';
+$mongo_user = '';
+$mongo_password = '';
+$mongo_dbname = 'gmail';
+$mongo_colname = 'backup';
 
+if($write_mongo) { 
+    print 'MongoDB url (default is '. $mogo_url . '): ';
+    trim(fgets(STDIN));
+    print 'MongoDB user: (default is '. $mongo_user . '): ';
+    trim(fgets(STDIN));
+    print 'MongoDB password: (default is '. $mongo_password . '): ';
+    trim(fgets(STDIN));
+    print 'MongoDB database: (default is '. $mongo_dbname . '): ';
+    trim(fgets(STDIN));
+    print 'MongoDB collection: (default is '. $mongo_colname . '): ';
+    trim(fgets(STDIN));
+}
 
 # Make dir if non-existent
 if (!file_exists($outputdir)) {
     mkdir($outputdir, 0777, true);
 }
 
-# Get mails data
+# Iterate email data
 if ($emails) {
     $total = count($emails);
     $remaining = $total;
+
+    if($write_mongo) {
+        $mongodb = new Mongo($mogo_url);
+        $mongodb_db = $mongodb->$mongo_dbname;
+        $mongodb_col = $mongodb_db->$mongo_colname;
+    }
+   
+
     foreach ($emails as $email_number) {
+        # Fetch body
         $message =  quoted_printable_decode(imap_fetchbody($inbox, $email_number, 1.1));
         if($message == '') {
             $message =  quoted_printable_decode(imap_fetchbody($inbox, $email_number, 1));
         }
+        # Fetch date, from, subject and message id
         $head = imap_fetch_overview($inbox, $email_number);
-    $head = $head[0];
+        $head = $head[0];
         $date = $head->date;
         $from = $head->from;
         $subject = $head->subject;
         $message_id = $head->message_id;
         $uid = $head->uid;
-    $email = array(
-        'message_id' => $message_id,
-        'uid' => $uid,
-        'date' => $date,
-        'subject' => $subject,
-        'from' => $from,
-        'message' => $message,
-    );
-        $per_remaining = sprintf ("%.2f", (1 - $remaining/$total) * 100);
-        print 'Exporting ' . $uid . ' of ' . $total . ' -> '.  $per_remaining .'%' . PHP_EOL;
+        $email = array(
+            'message_id' => $message_id,
+            'uid' => $uid,
+            'date' => $date,
+            'subject' => $subject,
+            'from' => $from,
+            'message' => $message,
+        );
+        # Output progress
+        $per_remaining = sprintf ("%.2f", $remaining/$total * 100);
+        print 'Processing message ' . $message_id . ' remaining '.  $per_remaining .'%' . PHP_EOL;
         $json_email = json_encode($email);
-        if($mails_array) {
-            $mails_array[] = $json_email;
-        }
-        else {
-            file_put_contents($outputdir . '/'. $uid . '_' . urlencode($message_id) . '.json', $json_email);
+        # Export to file and mongo
+        file_put_contents($outputdir . '/'. $uid . '_' . urlencode($message_id) . '.json', $json_email);
+        if($write_mongo) {
+	    $mongodb_col->save($email);
         }
         $remaining--;
     }
     imap_close($inbox);
-    if($mails_array) {
-        file_put_contents($outputdirr . '/' . $user . '.json', json_encode($mails_array));
+    # MongoDB indexing and output statistics
+    if($write_mongo) {
+        print 'Imported records: ';
     }
 } else {
     print 'No mails available' . PHP_EOL;
